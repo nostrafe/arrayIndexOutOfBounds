105d10f9f7077b24bc2d52d06d3332d70bee5342
nostrafe <38117561+nostrafe@users.noreply.github.com>
2020-05-13 15:03:07 -0400

    moved functions out of main
    also added comments to functions

---
+++ battleship.cpp
@@ -0,0 +1,78 @@
+//
+// Created by Henry on 5/12/2020.
+//
+#include "battleship.h"
+const std::string shipNames[] = {"Carrier","Battleship","Destroyer", "Submarine", "Patrol Boat"};
+const int shipValues[] = {5,4,3,3,2};
+
+void initializeBoard(int board[10][10]){
+    for(int x = 0; x < 10; x++){
+        for(int y = 0; y < 10; y++){
+            board[x][y] = 0;
+        }
+    }
+}
+void printBoard(int board[10][10]){
+    std::cout << "  A  B  C  D  E  F  G  H  I  J" << std::endl;
+    for(int x = 0; x < 10; x++){
+        std::cout << x + 1 << " ";
+        for(int y = 0; y < 10; y++){
+            std::cout << board[x][y] << "  ";
+        }
+        std::cout << std::endl;
+    }
+}
+void gameSetup(int board[10][10]){
+    int tempBoard[10][10];
+    copyArr(board, tempBoard);
+    int x1,x2,y1,y2;
+    for(int i = 0; i < 10; i++){
+        std::cout << "Where would you like to place the front of your " << shipNames[i] << "? (x1,y1)" << std::endl;
+            std::cin >> x1;
+            std::cin >> y1;
+
+        std::cout << "Where would you like to place the end of your " << shipNames[i] << "? (x2,y2)" << std::endl;
+        do{
+            std::cin >> x2;
+            std::cin >> y2;
+        } while(!legalPlacement(board, x1, y1, x2, y2, shipValues[i]));
+    }
+}
+bool legalPlacement(int board[10][10], int x1, int y1, int x2, int y2,  int shipLength){
+    if(x1 == x2){
+        for(int i = 0; i < y2-y1 ; i++){
+            if(board[x1][i] != 0) return false;
+        }
+    }// up and down
+    else if (y1 == y2){
+        for(int i = 0; i < x2-x1 ; i++){
+            if(board[i][y1] != 0) return false;
+        }
+    } // side to side
+    else{
+        std::cout << "Some other error (to be determined)" << std::endl;
+        return false;
+    }
+    return true;
+}
+
+void copyArr(int board1[10][10], int board2[10][10]){
+    for(int x = 0; x < 10; x++){
+        for(int y = 0; y < 10; y++){
+            board2[x][y] = board1[x][y];
+        }
+    }
+}
+
+void attack(int x, int y, int board[10][10], int * numHit){
+    int val = board[x][y];
+    switch(val){
+        case 0:
+            board[x][y] = 2;
+            break;
+        case 1:
+            board[x][y] = 3;
+            numHit++;
+            break;
+    }
+}
--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include "battleship.h"
 using namespace std;
 
 const int empty    = 0;
@@ -7,80 +8,39 @@ const int full     = 1;
 const int missed   = 2;
 const int hit      = 3;
 const int invalid  = 4;
-const string shipNames[] = {"Carrier","Battleship","Destroyer", "Submarine", "Patrol Boat"};
-const int shipValues[] = {5,4,3,3,2};
+
 int Player1[ 10 ][ 10 ];
 int Player2[ 10 ][ 10 ]; //temp/unused, can be used for the computer player
+int autoBoard1[10][10] = {{1,0,1,0,0,0,0,0,0,0},
+                              {1,0,1,0,0,0,0,0,0,0},
+                              {1,0,1,0,0,1,0,0,0,0},
+                              {1,0,1,0,0,1,0,0,0,0},
+                              {1,0,0,0,0,1,0,0,0,0},
+                              {0,0,0,0,0,0,0,0,0,0},
+                              {1,1,0,0,1,0,0,0,0,0},
+                              {0,0,0,0,1,0,0,0,0,0},
+                              {0,0,0,0,1,0,0,0,0,0}};
+int autoBoard2[10][10] = {{1,0,1,0,0,0,0,0,0,0},
+                          {1,0,1,0,0,0,0,0,0,0},
+                          {1,0,1,0,0,1,0,0,0,0},
+                          {1,0,1,0,0,1,0,0,0,0},
+                          {1,0,0,0,0,1,0,0,0,0},
+                          {0,0,0,0,0,0,0,0,0,0},
+                          {1,1,0,0,1,0,0,0,0,0},
+                          {0,0,0,0,1,0,0,0,0,0},
+                          {0,0,0,0,1,0,0,0,0,0}};
 
-void initializeBoard(int board[10][10]); // function takes a 10 by 10 array and populates it with values
-void printBoard(int board[10][10]); // prints a 10 by 10 array formatted as a battleship board
-void gameSetup(int board[10][10]); // sets up player boats on the board
-bool legalPlacement(int board[10][10], int x1, int y1, int x2, int y2, int shipSize); // checks to see if a boat is able to be placed
-void copyArr(int board1[10][10], int board2[10][10]); // used in gamesetup
+
 
 int main() {
-    initializeBoard(Player1);
+    int p1Hits = 0;
+    int p2Hits = 0;
     printBoard(Player1);
-    gameSetup(Player1);
+    for(int x = 0; x < 10; x++){
+        for(int y = 0; y < 10; y++){
+            attack(x,y,Player1);
+        }
+    }
     printBoard(Player1);
     return 0;
-}
-void initializeBoard(int board[10][10]){
-    for(int x = 0; x < 10; x++){
-        for(int y = 0; y < 10; y++){
-            board[x][y] = 0;
-        }
-    }
-}
-void printBoard(int board[10][10]){
-    cout << "  A  B  C  D  E  F  G  H  I  J" << endl;
-    for(int x = 0; x < 10; x++){
-        cout << x + 1 << " ";
-        for(int y = 0; y < 10; y++){
-            cout << board[x][y] << "  ";
-        }
-        cout << endl;
-    }
-}
-void gameSetup(int board[10][10]){
-    int tempBoard[10][10];
-    copyArr(board, tempBoard);
-    int x1,x2,y1,y2;
-    for(int i = 0; i < 10; i++){
-        cout << "Where would you like to place the front of your " << shipNames[i] << "? (x1,y1)" << endl;
-        do{
-            cin >> x1;
-            cin >> y1;
-        } while(board[x1][y1] == 0);
-        cout << "Where would you like to place the end of your " << shipNames[i] << "? (x2,y2)" << endl;
-        do{
-            cin >> x2;
-            cin >> y2;
-        } while(legalPlacement(board, x1, y1, x2, y2, shipValues[i]));
-    }
-}
-bool legalPlacement(int board[10][10], int x1, int y1, int x2, int y2,  int shipLength){
-    if(x1 == x2){
-        for(int i = 0; i < y2-y1 ; i++){
-            if(board[x1][i] != 0) return false;
-        }
-    }// up and down
-    else if (y1 == y2){
-        for(int i = 0; i < x2-x1 ; i++){
-            if(board[i][y1] != 0) return false;
-        }
-    } // side to side
-    else{
-        cout << "Some other error (to be determined)" << endl;
-        return false;
-    }
-    return true;
-}
-
-void copyArr(int board1[10][10], int board2[10][10]){
-    for(int x = 0; x < 10; x++){
-        for(int y = 0; y < 10; y++){
-            board2[x][y] = board1[x][y];
-        }
-    }
 }
